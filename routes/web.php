09273f8a7ba35d7e9be81a65d5e9aa11e7f5ea87
MD Rifat <mdrifatulislam59@gmail.com>
2023-08-03 11:07:18 +0600

    dashboard add

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('front');
 });
+Route::get('/admin', function () {
+    return view('admin');
+});
 
 Route::get('/dashboard', function () {
     return view('dashboard');
