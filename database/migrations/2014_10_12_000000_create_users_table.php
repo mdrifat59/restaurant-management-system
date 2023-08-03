76c24805041804af6d36d923899018be6e784501
MD Rifat <mdrifatulislam59@gmail.com>
2023-08-03 13:30:05 +0600

    role model add with show table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,9 +13,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->bigInteger("role_id")->unsigned();
+            $table->foreign('role_id')->references('id')->on('roles')->onDelete("cascade");
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('phone'); 
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
