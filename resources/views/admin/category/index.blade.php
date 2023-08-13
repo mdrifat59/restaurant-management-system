fe9a82b2d2f307076311b885c71792e5adb7633c
MD Rifat <mdrifatulislam59@gmail.com>
2023-08-13 12:04:45 +0600

    category edit and update complete

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -34,7 +34,7 @@
              {{$category->description}}
           </td>
           <td>{{$category->created_at->diffforhumans()}}</td>
-          <td><a href="#"><i class="bi bi-pencil-square text-info mr-2"></i></a> | <a href="#"><i class="bi bi-trash text-danger ml-2"></i></a></td>
+          <td><a href="{{route('categorys.edit', $category->id)}}" ><i class="bi bi-pencil-square text-info mr-2 "></i></a> | <a href="#"><i class="bi bi-trash text-danger ml-2"></i></a></td>
         </tr>
     @empty
         <tr>
@@ -76,4 +76,32 @@
     </div>
   </div>
 </div>
+{{-- <!-- Modal2 -->
+<div class="modal fade" id="editCategory" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="editCategoryLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered modal-lg">
+    <div class="modal-content">
+      <form action="{{ route('categorys.store') }}" method="post">
+        @csrf
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="editCategoryLabel">Category Add Form</h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Category Title:</label>
+            <input type="text" name="title" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+           </div>
+           <div class="mb-3">
+            <label for="" class="form-label">Description:</label>   <br>
+            <textarea name="description" id="" cols="50" rows="10"></textarea>
+           </div>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          <button type="submit" class="btn btn-primary">Submint</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</div> --}}
 @endsection
