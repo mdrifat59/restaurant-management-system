1ddf5e027c3e119638bdd6e384f313215a9db6b2
mdrifat59 <mdrifatulislam59@gmail.com>
2023-08-15 01:20:31 +0600

    status field add in user table

--- database/migrations/2023_08_03_070860_create_users_table.php
+++ database/migrations/2023_08_03_070860_create_users_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('phone'); 
+            $table->tinyInteger('status')->default(0);
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
