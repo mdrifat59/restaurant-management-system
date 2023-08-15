71a8c57e0894a462335095a6d48709f92fb87d4e
mdrifat59 <mdrifatulislam59@gmail.com>
2023-08-15 23:36:42 +0600

    user table ar status field delete

--- database/migrations/2023_08_03_070860_create_users_table.php
+++ database/migrations/2023_08_03_070860_create_users_table.php
@@ -18,8 +18,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('phone'); 
-            $table->tinyInteger('status')->default(0);
+            $table->string('phone');  
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
