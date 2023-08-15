7eeaa7a03d04a1065f8976b521def2fba1743eb4
Ara-fat74 <shourav8989@gmail.com>
2023-08-15 12:13:55 +0600

    Checkout page replaced

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -1,117 +1,128 @@
 @extends('layouts.master')
 
 @section('content')
-    <html>
-        <head>
-            <style>
-            .gradient-custom {
-            background: -webkit-linear-gradient(left, #3931af, #00c6ff);
-            }
+<html>
+  <head>
+    <style>
+    .text-font{
+      font-family: futura-pt,Tahoma,Geneva,Verdana,Arial,sans-serif;
+      font-weight: 700;
+      letter-spacing: .156rem;
+      font-size: 1.125rem;
+      }
+      .text-price{
+      padding: 0 .625rem;
+      font-family: futura-pt,Tahoma,Geneva,Verdana,Arial,sans-serif;
+      font-style: normal;
+      font-size: .75rem;
+      font-weight: 700;
+      line-height: .813rem;
+      letter-spacing: 1.6px;
+      }
+      .text-descriptions{
+      font-family: futura-pt,Tahoma,Geneva,Verdana,Arial,sans-serif;
+      font-style: normal;
+      font-size: .75rem;
+      font-weight: 400;
+      line-height: 1.125rem;
+      margin: .313rem 0 .938rem;
+      padding: 0 .625rem;
+      }
+      .button-color{
+      color: #4e4e4e ;
+      border-color: #4e4e4e ;
+      }
+      .button-order{
+      font-family: futura-pt,Tahoma,Geneva,Verdana,Arial,sans-serif;
+      font-style: normal;
+      font-size: .75rem;
+      font-weight: 700;
+      background-color: hsl(90, 40%, 50%);
+      color: white;}
+      </style>
+  </head>
+  <body>
+    <div class="container my-5 py-5">
 
-            .card-custom {
-            border-bottom-left-radius: 10% 50%;
-            border-top-left-radius: 10% 50%;
-            background-color: #f8f9fa ;
-            }
-
-
-            .input-custom {
-            background-color: white ;
-            }
-
-            .white-text {
-            color: hsl(52, 0%, 98%);
-            font-weight: 100 ;
-            font-size: 14px;
-            }
-
-            .back-button {
-            background-color: hsl(52, 0%, 98%);
-            font-weight: 700;
-            color: black ;
-            margin-top: 50px ;}
-            </style>
-        </head>
-        <body>
-            <div class="row mt-3 mx-3" style="margin-top:25px ;">
-                <div class="col-md-3">
-                  <div style="margin-top: 50px; margin-left: 10px;" class="text-center">
-                    <i id="animationDemo" data-mdb-animation="slide-right" data-mdb-toggle="animation"
-                      data-mdb-animation-reset="true" data-mdb-animation-start="onScroll"
-                      data-mdb-animation-on-scroll="repeat" class="fas fa-3x fa-shipping-fast text-white"></i>
-                    <h3 class="mt-3 text-white">Welcome</h3>
-                    {{-- <p class="white-text">You are 30 seconds away from compleating your order!</p> --}}
-                  </div>
-                  <div class="text-center">
-                    <a href="{{url('/cart')}}" class="btn btn-white btn-rounded back-button">Go back </a>
-                  </div>
-              
-              
-                </div>
-                <div class="col-md-9 justify-content-center">
-                  <div class="card card-custom pb-4">
-                    <div class="card-body mt-0 mx-5">
-                      <div class="text-center mb-3 pb-2 mt-3">
-                        <h4 style="color: #495057 ;">Delivery Details</h4>
+      <!--Section: Design Block-->
+      <section>
+    
+        <div class="row">
+          <div class="col-md-10 mb-6">
+            <div class="card mb-4">
+              <div class="card-header py-3">
+                <h5 class="mb-0 text-font text-uppercase">Delivery address</h5>
+              </div>
+              <div class="card-body">
+                <form>
+    
+                  <div class="row mb-4">
+                    <div class="col">
+                      <div class="form-outline">
+                        <input type="text" id="form11Example1" class="form-control" />
+                        <label class="form-label" for="form11Example1">First name</label>
                       </div>
-              
-                      <form class="mb-0">
-              
-                        <div class="row mb-4">
-                          <div class="col">
-                            <div class="form-outline">
-                              <input type="text" id="form9Example1" class="form-control input-custom" />
-                              <label class="form-label" for="form9Example1">First name</label>
-                            </div>
-                          </div>
-                          <div class="col">
-                            <div class="form-outline">
-                              <input type="text" id="form9Example2" class="form-control input-custom" />
-                              <label class="form-label" for="form9Example2">Last name</label>
-                            </div>
-                          </div>
-                        </div>
-                        <div class="row mb-4">
-                          <div class="col">
-                            <div class="form-outline">
-                              <input type="text" id="form9Example3" class="form-control input-custom" />
-                              <label class="form-label" for="form9Example3">City</label>
-                            </div>
-                          </div>
-                          <div class="col">
-                            <div class="form-outline">
-                              <input type="text" id="form9Example4" class="form-control input-custom" />
-                              <label class="form-label" for="form9Example4">Address</label>
-                            </div>
-                          </div>
-                        </div>
-                        <div class="row mb-4">
-                          <div class="col">
-                            <div class="form-outline">
-                              <input type="number" id="form9Example6" class="form-control input-custom" />
-                              <label class="form-label" for="form9Example6">Phone No.</label>
-                            </div>
-                          </div>
-                          <div class="col">
-                            <div class="form-outline">
-                              <input type="email" id="typeEmail" class="form-control input-custom" />
-                              <label class="form-label" for="typeEmail">Email</label>
-                            </div>
-                          </div>
-                        </div>
-              
-                        <div class="float-end ">
-                          <!-- Submit button -->
-                          {{-- <button type="submit" class="btn btn-primary btn-rounded"
-                            style="background-color: #0062CC ;">Place order</button> --}}
-                            <a href="{{url('/invoice')}}" class="btn btn-primary  btn-rounded back-button">Place order</a>
-                        </div>
-              
-                      </form>
+                    </div>
+                    <div class="col">
+                      <div class="form-outline">
+                        <input type="text" id="form11Example2" class="form-control" />
+                        <label class="form-label" for="form11Example2">Last name</label>
+                      </div>
                     </div>
                   </div>
-                </div>
+    
+                  <!-- Text input -->
+                  <div class="form-outline mb-4">
+                    <input type="text" id="form11Example4" class="form-control" />
+                    <label class="form-label" for="form11Example4">Address</label>
+                  </div>
+    
+                  <!-- Email input -->
+                  <div class="form-outline mb-4">
+                    <input type="email" id="form11Example5" class="form-control" />
+                    <label class="form-label" for="form11Example5">Email</label>
+                  </div>
+    
+                  <!-- Number input -->
+                  <div class="form-outline mb-4">
+                    <input type="number" id="form11Example6" class="form-control" />
+                    <label class="form-label" for="form11Example6">Phone</label>
+                  </div>
+    
+                  <!-- Message input -->
+                  <div class="form-outline mb-4">
+                    <textarea class="form-control" id="form11Example7" rows="4"></textarea>
+                    <label class="form-label" for="form11Example7">Additional information</label>
+                  </div>
+    
+                  <!-- Checkbox -->
+                  <div class="form-check d-flex justify-content-center mb-2">
+                    <input class="form-check-input me-2" type="checkbox" value="" id="form11Example8" checked />
+                    <label class="form-check-label" for="form11Example8">
+                      Create an account?
+                    </label>
+                  </div>
+                </form>
               </div>
-         </body>
-    </html>
+    
+            </div>
+            <div class="text-center">
+              <span><a href="{{url('/invoice')}}" class="btn btn-success btn-rounded back-button col-md-10">Place Order </a></span>
+            </div>
+    
+          </div>
+    
+    
+    
+    
+    
+    
+        </div>
+    
+      </section>
+      <!--Section: Design Block-->
+    
+    </div>
+  </body>
+</html>
 @endsection
