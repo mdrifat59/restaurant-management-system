5c5aededec8d0636c93f1a7d294d1b23f32c100e
mdrifat59 <mdrifatulislam59@gmail.com>
2023-08-15 23:45:54 +0600

    user seerder file ar status option delete

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,10 +15,10 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         $users=[
-            [ 'role_id' => 1, 'name' => 'Mohona Akter', 'email' => 'mohona@gmail.com', 'phone' => '+88 (014) 22-455656', 'status' => 1, 'password' => Hash::make('admin') ],
-            [ 'role_id' => 1, 'name' => 'Shorifa Akter', 'email' => 'shorifa@gmail.com', 'phone' => '+88 (015) 22-455656', 'status' => 1, 'password' => Hash::make('admin') ],
-            [ 'role_id' => 2, 'name' => 'John Doe', 'email' => 'admin@email.com', 'phone' => '+88 (012) 34-567890', 'status' => 1, 'password' => Hash::make('secret') ],
-            [ 'role_id' => 1, 'name' => 'Shawon Khan', 'email' => 'shawonk007@gmail.com', 'phone' => '+88 (016) 88-947741', 'status' => 1, 'password' => Hash::make('Dhaka1216') ],
+            [ 'role_id' => 1, 'name' => 'Mohona Akter', 'email' => 'mohona@gmail.com', 'phone' => '+88 (014) 22-455656',   'password' => Hash::make('admin') ],
+            [ 'role_id' => 1, 'name' => 'Shorifa Akter', 'email' => 'shorifa@gmail.com', 'phone' => '+88 (015) 22-455656',   'password' => Hash::make('admin') ],
+            [ 'role_id' => 2, 'name' => 'John Doe', 'email' => 'admin@email.com', 'phone' => '+88 (012) 34-567890',   'password' => Hash::make('secret') ],
+            [ 'role_id' => 1, 'name' => 'Shawon Khan', 'email' => 'shawonk007@gmail.com', 'phone' => '+88 (016) 88-947741',   'password' => Hash::make('Dhaka1216') ],
         ];
         foreach($users as $user){
             User::create($user);
