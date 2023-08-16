86da188dcbddd46d99723476e5ed913e9ec49034
mdrifat59 <mdrifatulislam59@gmail.com>
2023-08-17 01:34:04 +0600

    registration data not add to database user table

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -41,27 +41,28 @@ background: linear-gradient(to right, rgba(132, 250, 176, 1), rgba(143, 211, 244
                 @csrf
                 <div class="form-outline mb-4">
                     <label class="form-label" for="form3Example1cg">Your Name</label>
-                  <input type="text" id="form3Example1cg" class="form-control form-control-lg" />
+                  <input type="text" id="form3Example1cg" name="name" class="form-control form-control-lg" required autofocus autocomplete="name" />
                 </div>
 
                 <div class="form-outline mb-4">
                     <label class="form-label" for="form3Example3cg">Your Email</label>
-                  <input type="email" id="form3Example3cg" class="form-control form-control-lg" />
+                  <input type="email" id="form3Example3cg" name="email" class="form-control form-control-lg" required autocomplete="username"/>
                 </div>
 
                 <div class="form-outline mb-4">
-                    <label class="form-label" for="form3Example4cg">Password</label>
-                  <input type="password" id="form3Example4cg" class="form-control form-control-lg" />
+                    <label class="form-label" for="form3Example4cg">Password</label> 
+                  <input type="password" id="form3Example4cg" name="password" class="form-control form-control-lg" required autocomplete="new-password" />
                 </div>
 
                 <div class="form-outline mb-4">
                     <label class="form-label" for="form3Example4cdg">Repeat your password</label>
-                  <input type="password" id="form3Example4cdg" class="form-control form-control-lg" />
+                  <input type="password" id="form3Example4cdg" class="form-control form-control-lg" required autocomplete="new-password" />
                 </div>
 
                 <div class="d-flex justify-content-center">
                   <button type="submit"
                     class="btn btn-success btn-block btn-lg gradient-custom-4 text-body px-5">Registration</button>
+                    {{-- <a href="{{url('/login')}}" class="btn btn-success btn-block btn-lg gradient-custom-4 text-body px-5">Registration</a> --}}
                 </div>
 
                 <p class="text-center text-muted mt-5 mb-0">Have already an account? <a href="{{url('/login')}}"
