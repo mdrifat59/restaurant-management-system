fdcde9616e3dd405c989c3949f2b9f70fdcdf495
mdrifat59 <mdrifatulislam59@gmail.com>
2023-08-19 21:34:20 +0600

    migrate ar booking table modify

--- database/migrations/2023_08_09_031839_create_bookings_table.php
+++ database/migrations/2023_08_09_031839_create_bookings_table.php
@@ -15,11 +15,13 @@ return new class extends Migration
             $table->id();
             $table->bigInteger("user_id")->unsigned();
             $table->foreign('user_id')->references('id')->on('users')->onDelete("cascade"); 
+            $table->string('title');
+            $table->string('email');
+            $table->integer('phone');
             $table->date('date');
-            $table->time('start_time');
-            $table->time('end_time');
-            $table->string('status');
+            $table->time('start_time'); 
             $table->integer('participant');
+            $table->string('status')->default(1);
             $table->timestamps();
         });
     }
