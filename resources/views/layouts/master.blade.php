46b6569bdc9fc50dee68c8428b74c8a92e08fe37
mdrifat59 <mdrifatulislam59@gmail.com>
2023-08-22 00:34:29 +0600

    sslcommerz ar setup is complete

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -126,6 +126,18 @@ timer: 1500
   </script>
 
   @yield('script')
+  <!--  sslcommerz ar script file  -->
+  <script>
+    (function (window, document) {
+        var loader = function () {
+            var script = document.createElement("script"), tag = document.getElementsByTagName("script")[0];
+            script.src = "https://sandbox.sslcommerz.com/embed.min.js?" + Math.random().toString(36).substring(7);
+            tag.parentNode.insertBefore(script, tag);
+        };
+
+        window.addEventListener ? window.addEventListener("load", loader, false) : window.attachEvent("onload", loader);
+    })(window, document);
+</script>
 
 </body>
 
--- routes/web.php
+++ routes/web.php
@@ -69,6 +69,20 @@ route::prefix('admin')->group(function(){
         Route::resource('foods',FoodController::class);
 });
 
+// SSLCOMMERZ Start
+Route::get('/example1', [SslCommerzPaymentController::class, 'exampleEasyCheckout']);
+Route::get('/example2', [SslCommerzPaymentController::class, 'exampleHostedCheckout']);
+
+Route::post('/pay', [SslCommerzPaymentController::class, 'index']);
+Route::post('/pay-via-ajax', [SslCommerzPaymentController::class, 'payViaAjax']);
+
+Route::post('/success', [SslCommerzPaymentController::class, 'success']);
+Route::post('/fail', [SslCommerzPaymentController::class, 'fail']);
+Route::post('/cancel', [SslCommerzPaymentController::class, 'cancel']);
+
+Route::post('/ipn', [SslCommerzPaymentController::class, 'ipn']);
+//SSLCOMMERZ END
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
