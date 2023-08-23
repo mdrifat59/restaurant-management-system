597479905cb9d266906186d8e1906e54b25ae7f1
mdrifat59 <mdrifatulislam59@gmail.com>
2023-08-24 01:38:24 +0600

    booking table store setting

--- resources/views/book_table.blade.php
+++ resources/views/book_table.blade.php
@@ -9,36 +9,36 @@
         <p>Book a Table</p>
       </div>
 
-      <form action="#" method="post"  data-aos="fade-up">
-        
+      <form action="{{route('booking.store')}}" method="post">
+        @csrf
         <div class="row">
           <div class="col-lg-4 col-md-6 form-group">
-            <input type="text" name="title" class="form-control border border-warning bg-dark text-light border-2"   placeholder="Your Name "  >
+            <input type="text" name="title" class="form-control border border-warning bg-dark text-light border-1"   placeholder="Your Name "  >
           
           </div>
           <div class="col-lg-4 col-md-6 form-group mt-3 mt-md-0">
-            <input type="email" class="form-control border border-warning bg-dark text-light border-2" name="email"  placeholder="Your Email"  >
+            <input type="email" class="form-control border border-warning bg-dark text-light border-1" name="email"  placeholder="Your Email"  >
           
           </div>
           <div class="col-lg-4 col-md-6 form-group mt-3 mt-md-0">
-            <input type="text" class="form-control border border-warning bg-dark text-light border-2" name="phone"   placeholder="Your Phone"   >
+            <input type="text" class="form-control border border-warning bg-dark text-light border-1" name="phone"   placeholder="Your Phone"   >
           
           </div>
           <div class="col-lg-4 col-md-6 form-group mt-3">
-            <input type="datetime" name="date" class="form-control border border-warning bg-dark text-light border-2"   placeholder="Date"  >
+            <input type="datetime" name="date" class="form-control border border-warning bg-dark text-light border-1"   placeholder="Date"  >
           
           </div>
           <div class="col-lg-4 col-md-6 form-group mt-3">
-            <input type="time" class="form-control border border-warning bg-dark text-light border-2" name="start_time"   placeholder="date" >
+            <input type="time" class="form-control border border-warning bg-dark text-light border-1" name="start_time"   placeholder="Time" >
           
           </div>
           <div class="col-lg-4 col-md-6 form-group mt-3">
-            <input type="number" class="form-control border border-warning bg-dark text-light border-2" name="participant"   placeholder="# of people" >
+            <input type="number" class="form-control border border-warning bg-dark text-light border-1" name="participant"   placeholder="# of people" >
             
           </div>
         </div>
         <div class="form-group mt-3">
-          <textarea class="form-control border border-warning bg-dark text-light border-2" name="description" rows="5" placeholder="Message"></textarea>
+          <textarea class="form-control border border-warning bg-dark text-light border-1" name="description" rows="5" placeholder="Message"></textarea>
            
         </div> 
         <div class="text-center mt-3"><button type="submit" class="btn border border-warning bg-success-subtle border-2"  >Book a Table</button></div>
