c7a95238444ba23b87580a7d4cac2d94f2253d6c
mdrifat59 <mdrifatulislam59@gmail.com>
2023-08-27 01:07:53 +0600

    cart page not complete

---
+++ resources/views/cart.blade2.php
@@ -0,0 +1,261 @@
+@extends('layouts.master')
+
+
+ {{-- Section Start  --}}
+
+ @section('content')
+ @php
+     $cart_array=cartArray()
+ @endphp
+ <div class="py-5"></div>
+ <div>
+   <section class="h-100 h-custom">
+     <div class="container h-100 py-5">
+          <h3 >Your Cart Items</h3>
+          <div class="row d-flex justify-content-center align-items-center h-100 ">
+            <div class="col">
+              <div class="row">
+                <div class="col-9">
+                  <div class="table-responsive">
+                    <table class="table bg-light">
+                      <thead>
+                        <tr>
+                          <th scope="col" class="h5">Shopping Bag</th>
+                          <th scope="col">Category</th>
+                          <th scope="col">Quantity</th>
+                          <th scope="col">Price</th>
+                          <th scope="col">SubTotal</th>
+                          <th scope="col">Action</th>
+                        </tr>
+                      </thead>
+                      <tbody id="data">  
+
+                          @foreach ($cart_array as $cart_show)
+                              
+                         
+                          <th scope="row" class="border-bottom-0">
+                            <div class="d-flex align-items-center">
+                              <img src="https://i.imgur.com/Oj1iQUX.webp" class="img-fluid rounded-3"
+                                style="width: 120px;" alt="Book">
+                              <div class="flex-column ms-4">
+                                <p class="mb-2">{{$cart_show['name']}}</p>
+                                {{-- <p class="mb-0">{{$cart_show['description']}}</p> --}}
+                              </div>
+                            </div>
+                          </th>
+                          <td class="align-middle border-bottom-0">
+                            <p class="mb-0" style="font-weight: 500;">Paperback</p>
+                          </td>
+                          <td class="align-middle border-bottom-0">
+                            <div class="d-flex flex-row">
+                              {{-- <button class="btn btn-link px-2"
+                                onclick="this.parentNode.querySelector('input[type=number]').stepDown()">
+                                <i class="bi bi-dash btn btn-outline-info text-dark"></i>
+                              </button> --}}
+                              <button class="btn btn-link px-2" onclick="decreaseQuantity()">
+                                <i class="bi bi-dash btn btn-outline-info text-dark"></i>
+                            </button>
+                            
+                            <div class="qty form-control form-control-sm" style="width: 50px;" id="quantityDisplay">{{$cart_show['quantity']}}</div>
+                            
+                            <button class="btn btn-link px-2" onclick="increaseQuantity()">
+                                <i class="bi bi-plus btn btn-outline-info text-dark"></i>
+                            </button>
+                          </div>
+                            {{-- <div id="priceDisplay">{{$cart_show['price']}}</div> --}}
+                            
+          
+                              {{-- <input id="form1" min="0" name="quantity" value="1" type="number"
+                                class="form-control form-control-sm" style="width: 50px;" /> --}}
+                                {{-- <div class="qty form-control form-control-sm" style="width: 50px;">{{$cart_show['quantity']}}</div>
+          
+                              <button class="btn btn-link px-2"
+                                onclick="this.parentNode.querySelector('input[type=number]').stepUp()">
+                                <i class="bi bi-plus btn btn-outline-info text-dark"></i>
+                              </button> --}}
+                            
+                          </td>
+                          <td class="align-middle border-bottom-0">
+                            <p class="mb-0" style="font-weight: 500;">  {{$cart_show['price']}}</div> &#2547</p>
+                          </td>
+                          <td class="align-middle border-bottom-0">
+                            <p class="mb-0" style="font-weight: 500;"><div id="priceDisplay"> {{$cart_show['price']*$cart_show['quantity']}}</div> &#2547</p>
+                          </td> 
+                          <td class="align-middle border-bottom-0">
+                            <a class="mb-0 delete" href="{{url('/delete-cart/'.$cart_show['id'])}}" style="font-weight: 500;"><i class="bi bi-trash text-danger"></i> </a>
+                          </td> 
+                        </tr>
+                         @endforeach
+                      </tbody>
+                      <tfoot>
+                        <tr>
+                          <th colspan="3">
+                            Total Selected: <?= count($cart_array) ?>
+                          </th>
+                          <th colspan="2">
+                              Total : <span id="total"><div id="priceDisplay">{{Cart::getTotal()}} </div> &#2547</span>
+                          </th>
+                      </tr>
+                      </tfoot>
+                    </table>
+                  </div>
+                </div>
+                <!-- start commit -->
+                <div class="col-3 bg-secondary">
+                  <div class="">
+                    <div class="d-flex justify-content-between" style="font-weight: 500;">
+                      <p class="mb-2">Subtotal</p>
+                      <p class="mb-2">{{Cart::getTotal()}} &#2547</p>
+                    </div>
+    
+                    <div class="d-flex justify-content-between" style="font-weight: 500;">
+                      <p class="mb-0">Tax</p>
+                      <p class="mb-0">5 %</p>
+                    </div>
+                    <div class="d-flex justify-content-between" style="font-weight: 500;">
+                      <p class="mb-0">Total Tax</p>
+                      <p class="mb-0">{{Cart::getTotal()* 0.05}} &#2547</p>
+                    </div>
+    
+                    <hr class="my-4">
+    
+                    <div class="d-flex justify-content-between mb-4" style="font-weight: 500;">
+                      <p class="mb-2">Total (tax included)</p>
+                      <p class="mb-2">{{Cart::getTotal() +(Cart::getTotal())*0.05 }} &#2547</p>
+                    </div>
+     
+
+                    <div class="d-flex justify-content-between">
+                      
+                    <div class="text-center">
+                      <span><a href="{{url('/menu')}}" class="btn btn-dark btn-rounded back-button "> Back Menu</a></span>
+                      
+                    </div>
+                    <div class="text-center">
+                      <span><a href="{{url('/checkout')}}" class="btn btn-dark btn-rounded back-button">Checkout  </a></span>
+                      
+                    </div>
+                    </div>
+    
+                  </div>
+                </div>
+                
+
+                <!-- end commit -->
+              </div>
+               
+               
+      
+            </div>
+          </div>
+        </div>
+      </section>
+ </div>
+ 
+ <script>
+  // JavaScript code here
+
+  let quantity = {{$cart_show['quantity']}};
+  const price = {{$cart_show['price']}};
+  const subtotalDisplay = document.getElementById('subtotalDisplay');
+  const totalDisplay = document.getElementById('totalDisplay');
+  const quantityDisplay = document.getElementById('quantityDisplay');
+  const priceDisplay = document.getElementById('priceDisplay');
+
+  function updateSubtotalAndTotal() {
+      const subtotal = (quantity * price).toFixed(2);
+      const total = (subtotal * 0.05).toFixed(2); // Adding 5% tax
+      subtotalDisplay.textContent = subtotal;
+      totalDisplay.textContent = total;
+  }
+
+  function changeQuantity(change) {
+      if (quantity + change >= 1) {
+          quantity += change;
+          quantityDisplay.textContent = quantity; // Update quantity display
+          priceDisplay.textContent = (quantity * price).toFixed(2); // Update price display
+          updateSubtotalAndTotal();
+      }
+  }
+
+  // Initialize the display
+  updateSubtotalAndTotal();
+
+  function decreaseQuantity() {
+      changeQuantity(-1);
+  }
+
+  function increaseQuantity() {
+      changeQuantity(1);
+  }
+</script>
+ {{-- <script>
+  $(document).ready(function () {
+    let c = new Cart();
+    // c.emptyCart();
+    console.log(c.items)
+    $("#cartbadge").attr("value", c.totalItems());
+
+    $(".addCartIcon").click(function(){
+      $t = $(this);
+      let i = {
+        id: $t.data("pid"),
+        name: $t.data("pname"),
+        price: $t.data("pprice"),
+        quantity: 1,
+      };
+      c.addItem(i);
+      talk("Item Added To Cart");
+      Swal.fire({
+position: 'top-end',
+icon: 'success',
+title: 'Item Added To Cart',
+showConfirmButton: false,
+timer: 1500
+}).then(()=>{
+        $("#cartbadge").attr("value", c.totalItems());
+        console.log(c.items);
+      });
+      
+      // console.log($(this).data('pid'));
+    });
+  });
+</script> 
+<script>
+
+  $(document).ready(function () {
+      let c = new Cart();
+      let total = c.getTotalPrice();
+      $("#total").html(total);    
+      //console.table(c.items);
+      $items = "";
+      c.items.forEach(item => {
+          $items += "<tr>";
+          $items += "<td>"+item.id+"</td>";
+          $items += "<td>"+item.name+"</td>";
+          $items += "<td>"+item.quantity+"</td>";
+          $items += "<td>"+item.price+"</td>";            
+          $items += "<td> <a class='removeCart' data-pid='"+item.id+"'><i class='bi bi-trash'></i></a> </td>";
+          $items += "</tr>";
+      });        
+      $("#data").html($items);
+
+      //remove single cart item
+      $("#data").on("click",'a.removeCart',function(){
+          let cart = new Cart();
+          talk("Are you sure you want to remove the item?");
+          let c = confirm("Are you sure you want to remove");
+          if(c){
+              let pid = $(this).data("pid");
+              cart.removeItem(pid);
+              $(this).parent().parent().remove();
+              $("#total").html(cart.getTotalPrice());
+              talk("Item removed successfully from your cart");
+          }
+      });
+  });
+</script>  --}}
+     
+ @endsection
+
+ 
---
+++ resources/views/menu2.blade.php
@@ -0,0 +1,93 @@
+@extends('layouts.master')
+
+@section('title')
+  {{ __('Our Menu') }}
+@endsection
+
+<div class="py-5"></div>
+ 
+    <div class="container mt-5">
+        <h1 class="text-center" id="heading">Our Menu</h1>
+        <div class="underline mb-5"></div>
+        <div class="categories text-center mb-5">
+            <a href="#" class="btn btn-outline-warning mx-1">All</a>
+            @foreach ($categories as $category)
+              <a href="javascript:void(0)" class="btn btn-outline-warning mx-1">{{ $category->title }}</a>
+            @endforeach
+             
+        </div> 
+          <!-- card show -->
+        <div class="menu-items mt-5">
+            <div class="row">
+                @foreach ($food as $food)
+                <div class="col-6">
+                    <div class="card mb-3 item_data" id="breakfast" style="min-height: 150px;">
+                        <div class="row g-0">
+                            <div class="col-md-4 col-sm-12">
+                                <img src="{{ asset('storage/'.$food->thumbnail) }}" class="img-fluid rounded-start border border-warning border-2 " style="width: 100%; height:150px; object-fit: cover;" alt="...">
+                            </div> 
+                            <div class="col-md-8">
+                                <div class="card-body">
+                                    <!-- <h5 class="card-title d-inline">Card title <span class="price">$15.99</span></h5> -->
+                                    <div class="card-title">
+                                        <h5 class="d-inline">{{$food->name}}</h5>
+                                        <span class="price   float-end">Price: <span class="text-danger">{{$food->price}}</span></span>
+                                    </div>
+                                    <p class="card-text"> {{$food->description}}</p>
+                                    <div class="button-container" style="display: flex; justify-content: space-between; align-items: center">
+                                        {{-- <a href="javascript:void(0)" type="button" class="btn btn-outline-success addCartIcon"  data-pid="{{$food->id}}"
+                                          data-pname="{{$food->title}}"
+                                          data-pprice="{{$food->price}}"
+                                          data-pamount="1"><i class="bi bi-cart3 cartbadge" id="cartbadge" value="5"></i></a> --}}
+                                          <form action="{{url('add-to-cart')}}" method="POST">
+                                            @csrf
+                                            <div class="add-to-cart">
+                                              <input type="hidden" name="quantity" value="1">
+                                            <input type="hidden" name="id" value="{{$food->id}}">
+                                            <button class="add-to-cart-btn"><i class="bi bi-cart-check-fill"></i>Add To Cart</button>
+                                            </div>
+                                          </form>
+                                        <a href="{{url('/checkout')}}" type="button" class="btn btn-outline-success">Order</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+          @endforeach
+                
+                 
+            </div> 
+        </div>
+    </div>
+
+    {{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script> --}}
+    {{-- <script src="script.js"></script>  --}}
+
+ 
+        {{-- <script>
+            $(document).ready(function() {
+              // When the "Add to Cart" button is clicked
+              $(".btn-outline-success").click(function() {
+                var foodId = $(this).data("food-id"); // Get the food ID from the data attribute
+            
+                // Send the food ID to the server using Ajax
+                $.ajax({
+                  url: "/add-to-cart", // Replace with your server-side route
+                  method: "POST",
+                  data: {food_id: foodId},
+                  success: function(response) {
+                    // Handle the success response from the server
+                    console.log(response); // You can log the response for debugging purposes
+                    // You can also perform additional actions on the page if needed
+                  },
+                  error: function(error) {
+                    // Handle the error response from the server
+                    console.error(error); // Log the error for debugging purposes
+                  }
+                });
+              });
+            });
+            </script> --}}
+            
+     
